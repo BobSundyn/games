dbc9dff589fa6301b2910c3f50a2983c880ff221
Robert Sundyn <robertsundyn@hotmail.com>
2020-10-20 12:24:28 -0300

    finished

---
+++ js/logica.js
@@ -0,0 +1,21 @@
+//evento na pagina de god of war
+function mostraAntigo(imagem)
+{
+	imagem.src="imagens/kgrego.png";
+}
+
+function mostraNovo(imagem)
+{
+	imagem.src="imagens/knord.png";
+}
+
+//evento na pagina do assassins creed
+function mostraAltair(imagem)
+{
+	imagem.src="imagens/altair.png";
+}
+
+function mostraAlexios(imagem)
+{
+	imagem.src="imagens/alexios.png";
+}
